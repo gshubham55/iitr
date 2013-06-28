29847f150557fe212fa9753946cd62619a3b61b2
Shubham Gupta <gshubham55@gmail.com>
2013-06-29 02:44:23 +0530

    Added scrolling through arrow keys

--- js/main.js
+++ js/main.js
@@ -11,5 +11,40 @@ jQuery(window).ready(function($) {
   $('.vitaminimage').height(height-50);
   $('.tonyimage').height(height-50);
   $('.placementimage').height(height-50);
+  var curht=0;
+  var scroll_count = 0;
+  function upArrowPressed() {
+   // Your stuff here
+   if (scroll_count!=0)
+   {
+   var scrollto = curht-height;
+       // scrollto = 0;
+       scroll_count-=1;
+   $('html, body').animate({scrollTop: scrollto}, 500);
+   curht-=height;
+   }
+  }
 
+  function downArrowPressed() {
+     // Your stuff here
+     if(scroll_count<13)
+     { 
+         var scrollto = curht+height;
+         $('html, body').animate({scrollTop: scrollto}, 500);
+         curht+=height;
+         scroll_count+=1;
+     }
+  }
+
+  document.onkeydown = function(evt) {
+      evt = evt || window.event;
+      switch (evt.keyCode) {
+          case 38:
+              upArrowPressed();
+              break;
+          case 40:
+              downArrowPressed();
+              break;
+      }
+  };
 });
